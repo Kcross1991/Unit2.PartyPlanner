945c140b24b4d510a283dda957b7d57f29f4851d
root <root@Kacy>
2023-11-30 07:57:56 -0600

    update js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const COHORT = "2310-GHP-ET-WEB-FT-SF";
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-ET-WEB-FT-SF/events`;
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
 
 const state = {
-  event: [],
+  events: [],
 };
 
-const eventList = document.querySelector("#event");
+const eventList = document.querySelector("#events");
 const addeventForm = document.querySelector("#addevent");
 addeventForm.addEventListener("submit", addevent);
 
@@ -14,7 +14,7 @@ addeventForm.addEventListener("submit", addevent);
  */
 async function render() {
   await getEvents();
-  renderevent();
+  renderEvents();
 }
 render();
 
@@ -25,52 +25,41 @@ render();
 async function getEvents() {
   try {
     const response = await fetch(API_URL);
-    const events = await response.json();
-    console.log(events.data);
-    state.event = events.data;
-    console.log(state.event);
+    const json = await response.json();
+    console.log(response)
+    state.events = json.data;
   } catch (error) {
     console.log(error);
   }
 }
-function renderevent() {
-  console.log(state.event);
-  if (!state.event) {
-    eventList.innerHTML = `<p>No event.</p>`;
+function renderEvents() {
+  console.log(state.events);
+  if (!state.events.length) {
+    eventList.innerHTML = "<li>No event.</li>";
+    return;
   }
 
-  const eventcards = state.event.map((event) => {
-    const eventElement = document.createElement("div");
-    eventElement.classList.add("event-card");
-    const isoStr = event.date;
-    const date = new Date(isoStr);
-    const time = date.toTimeString();
-    eventElement.innerHTML = `
-    <div>Name: ${event.name}</div>
-    <div>Date: ${event.date}</div>
-    <div>Time: ${time} </div>
-    <div>Location: ${event.location} </div>
-    <div>Description: ${event.description}</div>
-    
-    `;
+  const eventcards = state.events.map((event) => {
+    const li = document.createElement("li");
+  
+    li.innerHTML = `
+     <h3>Name:${event.name}<h3>
+     <p>${event.date}</p>
+     <p>Location:${event.location}</p>
+    <p>Description: ${event.description}</p> `;
 
     //delete button
-    const deleteButton = document.createElement("button");
+    const deleteButton = document.createElement('button');
     deleteButton.textContent = "Delete Event";
-    eventElement.append(deleteButton);
+    li.append(deleteButton);
 
     deleteButton.addEventListener("click", () => deleteEvent(event.id));
 
-    //edit button
-    const editButton = document.createElement("button");
-    editButton.textContent = "Edit Event";
-    eventElement.append(editButton);
-
-    editButton.addEventListener("click", () => updateEvent(event.id));
-
-    return eventElement;
-  })
-  eventList.replaceChildren(...eventcards);
+    return li;
+  });
+  eventsList.replaceChildren(...eventcards)
+  console.log(eventcards)
+  
 }
 
 /**
@@ -79,19 +68,17 @@ function renderevent() {
  */
 
 async function createEvent(event) {
-  const time = addeventForm.time.value;
-  const date = addeventForm.date.value;
-  const datecombine = `${time} ${date}`;
-  const newdate = new Date(datecombine);
-  const fdate = newdate.toISOString();
-  eventvent.preventDefault();
+ 
+  event.preventDefault();
+
   try {
+    const addeventForm = document.querySelector("#addevent");
     const response = await fetch(API_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         name: addeventForm.name.value,
-        date: fdate,
+        date: addeventForm.date.value,
         location: addeventForm.location.value,
         description: addeventForm.description.value,
       }),
@@ -115,16 +102,16 @@ async function updateEvent(id) {
   const datecombine = `${time} ${date}`;
   const newdate = new Date(datecombine);
   const fdate = newdate.toISOString();
-  updateEvent.preventDefault();
+
   try {
-    const response = await fetch(API_URL, {
+    const response = await fetch(`${API_URL}/${id}`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        name: eventForm.name.value,
+        name: addeventForm.name.value,
         date: fdate,
-        location: eventForm.location.value,
-        description: eventForm.description.value,
+        location: addeventForm.location.value,
+        description: addeventForm.description.value,
       }),
     });
 
@@ -151,6 +138,4 @@ async function deleteEvent(id) {
   }
 }
 
-/**
- * Render events from state
- */
+
